94f4b872040ebba3ae662a1ac274dabc11355f99
Mauro Lepore <maurolepore@gmail.com>
2019-01-15 18:17:13 -0500

    Add files in workshop before rsconf starting overmorrow

---
+++ R/01_write-installed-packages.R
@@ -0,0 +1,32 @@
+## deja vu from earlier!
+
+## create a data frame of your installed packages
+## hint: installed.packages() is the function you need
+library(tidyverse)
+
+inst <- installed.packages() %>%
+  as_tibble()
+
+## optional: select just some of the variables, such as
+##   * Package
+##   * LibPath
+##   * Version
+##   * Priority
+##   * Built
+inst_sub <- inst %>%
+  select(
+    Package,
+    LibPath,
+    Version,
+    Priority,
+    Built
+  )
+
+## write this data frame to data/installed-packages.csv
+## hint: readr::write_csv() or write.table()
+## idea: try using here::here() to create the file path
+write_csv(inst_sub, here::here("data", "installed-packages.csv"))
+
+## YES overwrite the file that is there now (or delete it first)
+## that's a old result from me (Jenny)
+## it an example of what yours should look like and where it should go
